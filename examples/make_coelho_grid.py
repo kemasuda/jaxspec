c53d4729d31af58371b235c2f649f616188d7bee
Kento Masuda <k_masuda@dhcp184.ess.sci.osaka-u.ac.jp>
2024-08-18 19:47:23 +0900

    new grid examples

--- examples/make_coelho_grid.py
+++ examples/make_coelho_grid.py
@@ -9,7 +9,8 @@ from jaxspec.modelgrid import *
 
 #%% grid parameters
 teffs = np.arange(3500, 7001, 250)
-loggs = np.arange(1, 5.1, 1.)
+loggs = np.arange(3., 5.1, 1.)
+#loggs = np.arange(1, 5.1, 1.)
 #fehs = np.array([-1., -0.5, 0., 0.2, 0.5])
 fehs = np.array([-1., -0.5, 0., 0.5])
 alphas = np.array([0, 0.4])
@@ -29,12 +30,14 @@ print ("feh:", fehs)
 print ("alpha:", alphas)
 
 #%% input and output directories: IRD H-band
+wavgrid_length = 5000
 data_dir = "/Users/k_masuda/data/s_coelho05/"
-output_dir = "/Users/k_masuda/data/specgrid_irdh_coelho/"
+output_dir = "/Users/k_masuda/data/specgrid_irdh_coelho%d/"%wavgrid_length
 d = pd.read_csv("wavranges_ird_h.csv")
 wavfactor = 10. # nm -> AA
 air_or_vac = "vac"
 
+'''
 #%% input and output directories: GAOES-RV
 data_dir = "/Users/k_masuda/data/s_coelho05/"
 output_dir = "/Users/k_masuda/data/specgrid_gaoes_coelho/"
@@ -48,6 +51,7 @@ output_dir = "/Users/k_masuda/data/specgrid_hdsred_coelho/"
 d = pd.read_csv("wavranges_hds-red.csv")
 wavfactor = 1. 
 air_or_vac = "air"
+'''
 
 #%%
 wmargin = 5 # margin in AA
@@ -55,7 +59,7 @@ for i in range(1, len(d)):
     wmin_aa = int(d.iloc[i].wavmin*wavfactor) - wmargin
     wmax_aa = int(d.iloc[i].wavmax*wavfactor) + wmargin
     print ("#", wmin_aa, wmax_aa)
-    output = compute_grid_coelho(model_params, wmin_aa, wmax_aa, data_dir=data_dir, output_dir=output_dir, air_or_vac=air_or_vac)
+    output = compute_grid_coelho(model_params, wmin_aa, wmax_aa, data_dir=data_dir, output_dir=output_dir, air_or_vac=air_or_vac, fixed_wavgrid_length=wavgrid_length)
 
 
 # %%
--- examples/make_ispec_grid.py
+++ examples/make_ispec_grid.py
@@ -8,22 +8,36 @@ import pandas as pd
 from jaxspec.modelgrid import *
 
 #%% input and output directories
+#wavgrid_length = 5000
+wavgrid_length = 3500
+#wavgrid_length = 2500
 data_dir = "/Users/k_masuda/data/ispecgrid_hband_turbospectrum/"
-output_dir = "/Users/k_masuda/data/specgrid_irdh_turbospectrum/"
+output_dir = "/Users/k_masuda/data/specgrid_irdh_turbospectrum%d/"%wavgrid_length
+dwav = pd.read_csv("wavranges_ird_h.csv")
+wavfactor = 10. # nm -> AA
+air_or_vac = "vac"
+
+#%% irdh, wide logg
+wavgrid_length = 3500
+data_dir = "/Users/k_masuda/data/ispecgrid_hband_turbospectrum_widelogg/"
+output_dir = "/Users/k_masuda/data/specgrid_irdh_turbospectrum_widelogg%d/"%wavgrid_length
 dwav = pd.read_csv("wavranges_ird_h.csv")
 wavfactor = 10. # nm -> AA
 air_or_vac = "vac"
 
 #%% input and output directories: GAOES-RV
+'''
 data_dir = "/Users/k_masuda/data/ispecgrid_gaoes_turbospectrum/"
 output_dir = "/Users/k_masuda/data/specgrid_gaoes_turbospectrum/"
 dwav = pd.read_csv("wavranges_gaoes-rv.csv")
 wavfactor = 1. 
 air_or_vac = "air"
+'''
 
 #%%
 wmargin = 5 # margin in AA
 for i in range(len(dwav)):
     wmin, wmax = dwav.wavmin[i]*wavfactor - wmargin, dwav.wavmax[i]*wavfactor + wmargin
     print (wmin, wmax)
-    compute_grid_ispec(wmin, wmax, data_dir, output_dir, air_or_vac=air_or_vac)
+    #compute_grid_ispec(wmin, wmax, data_dir, output_dir, air_or_vac=air_or_vac)
+    compute_grid_ispec(wmin, wmax, data_dir, output_dir, air_or_vac=air_or_vac, wavgrid_length=wavgrid_length)
